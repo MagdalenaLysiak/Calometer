807f6d0f9eaf3609ca7c52caba7ae001cf5a95ac
Marcin Baryłka <marcin.barylka@gmail.com>
2016-10-30 10:45:12 +0100

    Poprawka Marcina

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,23 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 
 gulp.task('jshint', function() {
-return gulp.src('js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
+    return gulp.src('js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
 });
 
 gulp.task('sass', function() {
-return gulp.src('scss/*.scss').pipe(sass({errLogToConsole: true,
-                                          outputStyle: 'expanded',
-                                          sourceComments: 'map'
-                                         
-                                         })).pipe(gulp.dest('css'))
+    return gulp.src('scss/*.scss').pipe(sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded',
+        sourceComments: 'map'
+
+    })).pipe(gulp.dest('css'))
 });
 
-gulp.task('watch', function(){
-gulp.watch('scss/*.scss', ['sass']);
-    
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+
 
 });
+
+
+gulp.task('default', ['watch']);
